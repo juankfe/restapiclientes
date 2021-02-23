0b3e5dfa93b67b63d41945d9739d73c5b6cbebeb
Juan OKeeffe <juan.okeeffe@gmail.com>
2021-02-22 21:32:47 -0300

    msg

--- src/main/java/restapiclientes/RestapiclientesApplication.java
+++ src/main/java/restapiclientes/RestapiclientesApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -18,9 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class RestapiclientesApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestapiclientesApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiclientesApplication.class, args);
 	}
-
+	
 }
